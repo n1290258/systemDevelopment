839384e618e3ffcfe1242f8e59c9172c244600c8
n1290258 <bjsk1290258@gn.iwasaki.ac.jp>
2020-09-16 11:09:32 +0900

    練習02_1

---
+++ src/jp/ac/isc/cloud/UserUpdateServlet.java
@@ -0,0 +1,41 @@
+package jp.ac.isc.cloud;
+
+import java.io.*;
+import java.sql.*;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.*;
+
+@WebServlet("/UserUpdateServlet")
+public class UserUpdateServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+			Connection users = null;
+			try {
+				users = DBConnection.openConnection();
+				String id = request.getParameter("updateId");
+				String name = request.getParameter("updateName");
+				String picture = request.getParameter("updatePicture");
+				Statement state = users.createStatement();
+				if(name.length() !=0) {
+					state.executeUpdate("UPDATE user_table SET name='" + name + "' WHERE id ='" + id + "'");
+				}
+				if(picture.length() != 0) {
+					state.executeUpdate("UPDATE user_table SET picture='" + picture + "' WHERE id ='" + id + "'");
+				}
+				DBConnection.closeConnection(users, state);
+				response.sendRedirect("/select"); //UserSelectServletを呼び出す
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+	}
+
+}
